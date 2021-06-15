e55e9b5ad517cd54c57b9711b0001a8b9ef0be6b
TheDude61636 <thedude61636@gmail.com>
2021-06-15 20:37:14 +0300

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,9 +11,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "4f186865a8b6b4b12b955e4281852918",
-"/": "4f186865a8b6b4b12b955e4281852918",
-"main.dart.js": "5d4272aa1c2b0fe4e9b2b4ede25f82fb",
+"index.html": "c9f908b74581fd7f6cca5ba5cad4f71d",
+"/": "c9f908b74581fd7f6cca5ba5cad4f71d",
+"main.dart.js": "86d6fe27c7955a3c1c6f2d73cacf2c13",
 "manifest.json": "b875e34d87a5646dcb8495ab8fe93269",
 "version.json": "22287f43e26f179b7d1560038aadbeae"
 };
